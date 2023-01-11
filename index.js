3834bdebfd84ada04222fe1c44dbeb6b236b1716
dusanisakov97 <dusisakov@gmail.com>
2023-01-11 11:00:45 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,80 @@
+// Accordion component open - close
+
+function accordion() {
+  const element = document.getElementsByClassName("item");
+  const contents = document.getElementsByClassName("hidden-box");
+
+  for (let i = 0; i < element.length; i++) {
+    element[i].addEventListener("click", function () {
+      if (contents[i].style.display == "block") {
+        contents[i].style.display = "none";
+      } else {
+        contents[i].style.display = "block";
+      }
+      element[i].classList.toggle("open");
+    });
+  }
+}
+
+accordion();
+
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
